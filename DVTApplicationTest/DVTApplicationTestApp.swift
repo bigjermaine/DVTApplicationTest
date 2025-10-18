416373f55848d60a97d869256bf034ad11e239d5
jermaine-iris <danieljermaine97@gmail.com>
2025-10-18 17:11:20 +0100

    Initial Commit

---
+++ DVTApplicationTest/DVTApplicationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVTApplicationTestApp.swift
+//  DVTApplicationTest
+//
+//  Created by Daniel Jermaine on 18/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct DVTApplicationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
